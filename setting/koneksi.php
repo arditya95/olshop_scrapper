ee87c1f21f6a1689df632ce0ed4219c7fccc06f7
Arditya Darmawan <putuarditya@gmail.com>
2017-05-16 22:46:21 +0800

    Start
    New Update

---
+++ setting/koneksi.php
@@ -0,0 +1,17 @@
+<?php
+// KONFIGURASI DATABASE
+$db_host="localhost";
+$db_user="root";
+$db_pass="";
+$db_name="compare_it";
+
+$con = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+
+if (mysqli_connect_errno()) {
+  	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+else {
+   echo "Connection Successful \n";
+   echo '<br>';
+}
+?>
