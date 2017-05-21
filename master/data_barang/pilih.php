5716bc3f6795632d82f287c81246b20ab55c3a9f
Arditya Darmawan <putuarditya@gmail.com>
2017-05-22 00:07:47 +0800

    test 2 atom git plus

--- master/data_barang/pilih.php
+++ master/data_barang/pilih.php
@@ -1,3 +1,4 @@
 <a href="index.php?kode=1" target="_self"> Barang </a>
 <a href="index.php?kode=2" target="_self"> Toko </a>
 <a href="index.php?kode=3" target="_self"> Toko </a>
+<a href="index.php?kode=4" target="_self"> Toko </a>
