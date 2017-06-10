5b56e9b4a94c3393cf828bc442a569441c08d6b2
Arditya Darmawan <putuarditya@gmail.com>
2017-06-10 23:53:11 +0800

    FTP Update

--- setting/koneksi.php
+++ setting/koneksi.php
@@ -1,9 +1,15 @@
 <?php
-// KONFIGURASI DATABASE
-$db_host="localhost";
-$db_user="root";
-$db_pass="";
-$db_name="compare_it";
+// [SERVER] KONFIGURASI DATABASE
+$db_host="mysql.idhostinger.com";
+$db_user="u294221690_toko";
+$db_pass="compareit";
+$db_name="u294221690_toko";
+
+// [LOCAL] KONFIGURASI DATABASE
+// $db_host="localhost";
+// $db_user="root";
+// $db_pass="";
+// $db_name="compare_it";
 
 $con = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 
